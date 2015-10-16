db17db208f9cdadede7a244026a73ce24eae36b0
melvin <melvin@melvin-VirtualBox>
2015-10-15 23:15:55 -0600

    paralelizado a cachete

--- main.cpp
+++ main.cpp
@@ -77,10 +77,14 @@ void inverseMapper(int *ZX, float *result) {
 
 }
 
-void convertImage(int ***salida, int ***entrada, int width, int height, int depth) {
+int ***entrada;
+int ***salida;
+int ***salida2;
+
+void convertImage(int width, int height, int depth) {
 
     int i, j, k;
-    
+    #pragma omp parallel for private(i,j,k) shared(entrada,salida)
     for (i = 0; i < width; i++) {
         for (j = 0; j < height; j++) {
 
@@ -108,15 +112,51 @@ void convertImage(int ***salida, int ***entrada, int width, int height, int dept
     }
 }
 
-int main(int argc, char** argv) {
+void suavizado(int width, int height, int depth) {
 
+    float gauss [3][3] = {
+        {0.01, 0.08, 0.01},
+        {0.08, 0.56, 0.08},
+        {0.01, 0.08, 0.01}
+    };
+
+
+    int i, j, k;
+    #pragma omp parallel for private(i,j,k) shared(salida,salida2)
+    for (i = 1; i < width - 1; i++) {
+        for (j = 1; j < height - 1; j++) {
+            for (k = 0; k < depth; k++) {
+
+                int p11 = salida[i - 1][j - 1][k];
+                int p12 = salida[i][j - 1][k];
+                int p13 = salida[i + 1][j - 1][k];
+                int p21 = salida[i - 1][j][k];
+                int p22 = salida[i][j][k];
+                int p23 = salida[i + 1][j][k];
+                int p31 = salida[i - 1][j + 1][k];
+                int p32 = salida[i][j + 1][k];
+                int p33 = salida[i + 1][j + 1][k];
+
+                int z =
+                        (p11 * gauss[0][0] + p12 * gauss[0][1] + p13 * gauss[0][2]
+                        + p21 * gauss[1][0] + p22 * gauss[1][1] + p23 * gauss[1][2]
+                        + p31 * gauss[2][0] + p32 * gauss[2][1] + p33 * gauss[2][2]);
+
+                salida2[i][j][k] = z;
+            }
+        }
+    }
+
+}
+
+int main(int argc, char** argv) {
 
     FreeImage_Initialise();
     atexit(FreeImage_DeInitialise);
 
 
-    FREE_IMAGE_FORMAT formato = FreeImage_GetFileType("foto2.jpg", 0);
-    FIBITMAP* bitmap = FreeImage_Load(formato, "foto2.jpg");
+    FREE_IMAGE_FORMAT formato = FreeImage_GetFileType("foto1.jpg", 0);
+    FIBITMAP* bitmap = FreeImage_Load(formato, "foto1.jpg");
 
     FIBITMAP* temp = FreeImage_ConvertTo32Bits(bitmap);
     int width = FreeImage_GetWidth(temp);
@@ -133,7 +173,7 @@ int main(int argc, char** argv) {
 
 
 
-    int ***entrada = new int**[Dim1];
+    entrada = new int**[Dim1];
     for (int i(0); i < Dim1; i++)
         entrada[i] = new int*[Dim2];
 
@@ -142,13 +182,21 @@ int main(int argc, char** argv) {
             entrada[i][j] = new int[Dim3];
 
 
-    int ***salida = new int**[Dim1];
+    salida = new int**[Dim1];
     for (int i(0); i < Dim1; i++)
         salida[i] = new int*[Dim2];
 
     for (int i(0); i < Dim1; i++)
         for (int j(0); j < Dim2; j++)
             salida[i][j] = new int[Dim3];
+
+    salida2 = new int**[Dim1];
+    for (int i(0); i < Dim1; i++)
+        salida2[i] = new int*[Dim2];
+
+    for (int i(0); i < Dim1; i++)
+        for (int j(0); j < Dim2; j++)
+            salida2[i][j] = new int[Dim3];
 
 
 
@@ -175,7 +223,8 @@ int main(int argc, char** argv) {
     start_time = omp_get_wtime();
 
     // generar la matrix de salida
-    convertImage(salida, entrada, width, height, 3);
+    convertImage(width, height, 3);
+    suavizado(width, height, 3);
     run_time = omp_get_wtime() - start_time;
     printf("%lf\n", run_time);
 
@@ -185,9 +234,9 @@ int main(int argc, char** argv) {
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
             RGBQUAD color;
-            color.rgbBlue = salida[i][j][0];
-            color.rgbGreen = salida[i][j][1];
-            color.rgbRed = salida[i][j][2];
+            color.rgbBlue = salida2[i][j][0];
+            color.rgbGreen = salida2[i][j][1];
+            color.rgbRed = salida2[i][j][2];
             FreeImage_SetPixelColor(new_bitmap, i, j, &color);
         }
     }
@@ -220,5 +269,7 @@ int main(int argc, char** argv) {
     FreeImage_Unload(bitmap);
 
     return 0;
+
+
 }
 
